f00e2ffef4ad11a471d4ae198aa561ca6e242642
emanuelirossetto <159554718+emanuelirossetto@users.noreply.github.com>
2024-09-12 08:54:49 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,58 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'historia',
+    'Quem foi a primeira pessoa a viajar no espaço?',
+    'Yuri Gagarin'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Historia',
+    'O feudalismo é de qual periodo?',
+    'Pré história'
+)
+criaCartao(
+)   'Geografia',
+    'Qual o maior pais do mundo',
+    'Russia',
+
+
+criaCartao(
+    'ingles',
+    'Aonde foi criado o ingles?',
+    'Inglaterra'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Portugues',
+    'De onde é a invenção do chuveiro eletrico?',
+    'Brasil'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Historia',
+    'Qual o menor pais do mundo?',
+    'Vaticano'
+)
+criaCartao(
+   'Historia',
+    'Qual livro mais vendido no mundo?',
+    'A bíblia'
+)
+
+criaCartao(
+    'Portugues',
+    'Quantos litros de sangue uma pessoa tem?',
+    'Entre 4 a 6 litros',
+)
+
+criaCartao(
+    'Matematica',
+    'Quantas casas decimais tem o numero PI',
+    'Infinitas'
+
+)
+
+criaCartao(
+    'Quimica',
+    'Quantos elementos quimicos tem na tabela periodica?',
+    '118',
 )
